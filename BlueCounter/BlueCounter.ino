786ed4d8929ffa9ec301fbff6388c3e6907a6580
Mihai Zecheru <zecheruchris@gmail.com>
2024-03-28 12:10:06 -0700

    Add radio receiver code & add end game funcs

--- BlueCounter/BlueCounter.ino
+++ BlueCounter/BlueCounter.ino
@@ -15,9 +15,6 @@ RF24 radio(7, 8); // CE = 7, CSN = 8
 Button blue_btn(3);
 //Button white_btn(4);
 
-uint8_t blue_score = 0;
-uint8_t red_score = 0;
-
 /**
  * Add one to the blue player's score. For when the blue button is pressed once
  */
@@ -45,12 +42,29 @@ void setup()
   radio.openWritingPipe(RED_RADIO_HOME_ADDRESS);
   radio.openReadingPipe(1, BLUE_RADIO_HOME_ADDRESS);
   radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+  radio.startListening(); // the radio is by default in listening mode, switching only to send info for a quick second
 }
 
 void loop()
 {
+  // The only transmissions that will be received is what the red player can send
+  switch (GetRadioTransmission(&radio))
+  {
+    case RadioMessage::invalid_or_none:     return;
+    case RadioMessage::increment_red_score: red_score++;
+    case RadioMessage::decrement_red_score: red_score--;
+    case RadioMessage::toggle_score_mode:   ToggleScoreMode();
+    case RadioMessage::start_new_game:      StartNewGame();
+  }
+
+  // Show the updated scores
   DisplayScores(&score_display, blue_score, red_score);
+
+  // Listen for button presses and send events if they one of the buttons is pressed
   blue_btn.CheckForPressOrHold(send_increment_blue_score_message, send_decrement_blue_score_message, &radio);
 //  white_btn.CheckForPressOrHold(send_start_new_game_message, send_toggle_score_mode_message, &radio);
+
+  // If a player has won, show that and end the game
+  if (HasWon(blue_score)) ShowWin(&score_display);
+  if (HasWon(red_score))  ShowLoss(&score_display);
 }
