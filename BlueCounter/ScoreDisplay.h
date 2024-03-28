19027dc8582a1e2a2695f3c4e7daa2a888170652
Mihai Zecheru <zecheruchris@gmail.com>
2024-03-28 12:11:38 -0700

    Add score & end-game funcs

--- BlueCounter/ScoreDisplay.h
+++ BlueCounter/ScoreDisplay.h
@@ -4,8 +4,8 @@
 #include <TM1637.h>
 
 /**
- * Blue and Red players' scores
- */
+   Blue and Red players' scores
+*/
 uint8_t blue_score = 0;
 uint8_t red_score = 0;
 
@@ -17,8 +17,8 @@ void StartNewGame()
 }
 
 /**
- * Whether the ping pong game is to be played to 11 or to 21
- */
+   Whether the ping pong game is to be played to 11 or to 21
+*/
 enum ScoreMode
 {
   ToEleven = 1,
@@ -28,8 +28,8 @@ enum ScoreMode
 ScoreMode SCORE_MODE = ScoreMode::ToEleven; // Game to 11 is the default
 
 /**
- * Toggle what score the game is played to, either to 11 or to 21
- */
+   Toggle what score the game is played to, either to 11 or to 21
+*/
 void ToggleScoreMode()
 {
   if (SCORE_MODE == ScoreMode::ToEleven)
@@ -39,13 +39,13 @@ void ToggleScoreMode()
 }
 
 /**
- * The brightness of the score_display. Integer between 0 - 7
- */
+   The brightness of the score_display. Integer between 0 - 7
+*/
 const uint8_t SCORE_DISPLAY_BRIGHTNESS = 5;
 
 /**
- * Initialize the given _score_display module and set it's brightness to the SCORE_DISPLAY_BRIGHTNESS constant
- */
+   Initialize the given _score_display module and set it's brightness to the SCORE_DISPLAY_BRIGHTNESS constant
+*/
 void InitScoreDisplay(TM1637* _score_display)
 {
   _score_display->init();
@@ -53,31 +53,54 @@ void InitScoreDisplay(TM1637* _score_display)
 }
 
 /**
- * Display both the blue and red players' scores on the given _score_display module
- * 
- * Note: this function must be called in a loop
- */
+   Display both the blue and red players' scores on the given _score_display module
+
+   Note: this function must be called in a loop
+*/
 void DisplayScores(TM1637* _score_display, uint8_t score_blue, uint8_t score_red)
 {
   /**
-   * If the score is 6, floor(6 / 10) is 0 (the first digit, i.e. the tens digit),
-   * and (6 % 10) is 6 (the second digit, i.e. the ones digit), giving "06"
-   */
+     If the score is 6, floor(6 / 10) is 0 (the first digit, i.e. the tens digit),
+     and (6 % 10) is 6 (the second digit, i.e. the ones digit), giving "06"
+  */
   _score_display->display(0, floor(score_blue / 10)); // Get the "ones" digit
   _score_display->display(1, score_blue % 10); // Get the "tens" digit
-  
+
   _score_display->point(true); // Turn on the colon separator
-  
+
   _score_display->display(2, floor(score_red / 10)); // Get the "ones" digit
   _score_display->display(3, score_red % 10); // Get the "tens" digit
 }
 
+/**
+   Check if the given player_score is high enough to win the game
+*/
 bool HasWon(uint8_t player_score)
 {
   // Use >= just in case of some bug possibly taking place
   if (SCORE_MODE == ScoreMode::ToEleven && player_score >= 11) return true;
-  else if (SCORE_MODE == Scoremode::ToTwentyOne && player_score >= 21) return true;
+  else if (SCORE_MODE == ScoreMode::ToTwentyOne && player_score >= 21) return true;
   return false;
+}
+
+const uint8_t SEVSEG_u = 0x3c;
+const uint8_t SEVSEG_L = 0x38;
+
+/**
+   Show to the player that he has won by displaying a double 'u'
+*/
+void ShowWin(TM1637* _score_display)
+{
+  _score_display->setCustomChar(0, SEVSEG_u);
+  _score_display->setCustomChar(1, SEVSEG_u);
+}
+
+/**
+   Show to the player that he has lost by displaying an 'L'
+*/
+void ShowLoss(TM1637* _score_display)
+{
+  _score_display->setCustomChar(0, SEVSEG_L);
 }
 
 #endif
