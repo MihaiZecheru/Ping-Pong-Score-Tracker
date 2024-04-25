1226f65e5e86b6980ac77f4770947db139bdb61f
Mihai Zecheru <zecheruchris@gmail.com>
2024-04-24 18:52:15 -0700

    Add Player.h import, fixes bug

--- ping_pong_score_tracker_library/ping_pong_score_tracker_library_src/ServeIndicator.h
+++ ping_pong_score_tracker_library/ping_pong_score_tracker_library_src/ServeIndicator.h
@@ -1,6 +1,8 @@
 #ifndef SERVE_INDICATOR_H
 #define SERVE_INDICATOR_H
 
+#include "Player.h"
+
 class ServeIndicator
 {
 private:
@@ -36,10 +38,10 @@ Player GetCurrentServer(Player starting_server, Player other_server)
 {
   // From ScoreIndicator.h
   int total_score = blue_score + red_score;
-  
+
   // The amount of times the server has changed is the amount of sets of 5 points that make up the total score
   int server_changes_count = floor(total_score / 5);
-  
+
   // Get the current server
   // If the server has switched twice, it's back to the original server
   return server_changes_count % 2 == 0 ? starting_server : other_server;
